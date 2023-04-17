db099c3bc2689abddaac96c0ad440b30181d8013
Gautam Bhat <gautam.kumar@spryker.com>
2023-04-17 15:28:44 +0200

    adding python script

--- .github/workflows/check-open-pr.py
+++ .github/workflows/check-open-pr.py
@@ -1,1 +1,7 @@
-print("hello from python")
+import requests
+import json
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+open_pull_requests = requests.get('https://api.github.com/repos/gka038/customer1/pulls?state=all', headers=headers)
+
+print(open_pull_requests)
