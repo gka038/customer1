e4a24ba02339f2f21b0b321fa830db267e75a3cd
Gautam Bhat <gautam.kumar@spryker.com>
2023-04-17 15:53:57 +0200

    with failing python error message

--- .github/workflows/check-open-pr.py
+++ .github/workflows/check-open-pr.py
@@ -5,7 +5,13 @@ import sys
 current_pr_number = sys.argv[1]
 
 headers = {'Accept': 'application/vnd.github.v3+json'}
-open_pull_requests = requests.get('https://api.github.com/repos/gka038/customer1/pulls?state=all', headers=headers)
+open_pull_requests = requests.get('https://api.github.com/repos/gka038/customer1/pulls?state=open', headers=headers).json()
 
 print("current PR number is :", current_pr_number)
-print(open_pull_requests)
+
+for obj in open_pull_requests:
+    if obj['number'] == int(current_pr_number):
+        open_pull_requests.pop()
+
+if len(open_pull_requests) > 0:
+    sys.exit('There are other open PRs on ths repo.. please wait for them to be resolved or closed')
