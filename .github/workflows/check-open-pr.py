cc14a9f0b9d422efdbbfa09295445a0707bd2f71
Gautam Bhat <gautam.kumar@spryker.com>
2023-04-17 15:38:51 +0200

    with pr number

--- .github/workflows/check-open-pr.py
+++ .github/workflows/check-open-pr.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import sys
+
+current_pr_number = sys.argv[1]
 
 headers = {'Accept': 'application/vnd.github.v3+json'}
 open_pull_requests = requests.get('https://api.github.com/repos/gka038/customer1/pulls?state=all', headers=headers)
 
+print("current PR number is :", current_pr_number)
 print(open_pull_requests)
